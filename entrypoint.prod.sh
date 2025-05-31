28b6e194c086f012de6c44ac621fef638d2b79b5
kofnet002 <noeljoel61@gmail.com>
2025-05-31 15:51:57 +0000

    add migration command to entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
+    while ! nc -z "$DB_HOST" "$DB_PORT"; do
       sleep 0.1
     done
 
-    echo "PostgreSQL started"
+    echo "PostgreSQL is up - running migrations..."
+
+    python manage.py migrate --noinput
+    python manage.py collectstatic --noinput
 fi
 
 exec "$@"
